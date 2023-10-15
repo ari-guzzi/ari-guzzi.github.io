8dbc6566fcc5d928bba15c46acdbe4bc74dd2189
Ari Guzzi <argu4451@colorado.edu>
2023-10-14 18:49:45 -0600

    its perfect

--- javascript/loadpage.js
+++ javascript/loadpage.js
@@ -2,7 +2,7 @@ function loadPage(page) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("content").innerHTML = this.responseText;
+            document.body.innerHTML = this.responseText;
         }
     };
     xhttp.open("GET", page + ".html", true);
