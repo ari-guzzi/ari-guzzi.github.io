1083773e3ca19895974d7d15f93f68f166157c06
Ari Guzzi <argu4451@colorado.edu>
2024-12-11 18:51:14 -0700

    Update myWorkJS.js

--- javascript/myWorkJS.js
+++ javascript/myWorkJS.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const works = [
-        {"link": "webdev.html", "imgSrc": "../images/coverImages/webdev.png", "altText": "Web Development", "description": "Web Development"},
-     {"link": "appdev.html", "imgSrc": "../images/coverImages/appdev.png", "altText": "App Development", "description": "App Development"},
-     {"link": "vrdev.html", "imgSrc": "../images/coverImages/vrdev.png", "altText": "VR Development", "description": "VR Development"},
-      {"link": "game.html", "imgSrc": "../images/coverImages/bayoubolt1.png", "altText": "Game Development", "description": "Tiny Game Development"},
-      {"link": "web.html", "imgSrc": "../images/coverImages/webdes.png", "altText": "Web Design", "description": "Web Design"},
-      {"link": "appdes.html", "imgSrc": "../images/coverImages/appdes.png", "altText": "App Design", "description": "App Design"},
-      {"link": "myhobbies.html", "imgSrc": "../images/coverImages/hobbies.png", "altText": "My Hobbies", "description": "My Hobbies"}
+        {"link": "webdev.html", "imgSrc": "../images/coverimages/webdev.png", "altText": "Web Development", "description": "Web Development"},
+     {"link": "appdev.html", "imgSrc": "../images/coverimages/appdev.png", "altText": "App Development", "description": "App Development"},
+     {"link": "vrdev.html", "imgSrc": "../images/coverimages/vrdev.png", "altText": "VR Development", "description": "VR Development"},
+      {"link": "game.html", "imgSrc": "../images/coverimages/bayoubolt1.png", "altText": "Game Development", "description": "Tiny Game Development"},
+      {"link": "web.html", "imgSrc": "../images/coverimages/webdes.png", "altText": "Web Design", "description": "Web Design"},
+      {"link": "appdes.html", "imgSrc": "../images/coverimages/appdes.png", "altText": "App Design", "description": "App Design"},
+      {"link": "myhobbies.html", "imgSrc": "../images/coverimages/hobbies.png", "altText": "My Hobbies", "description": "My Hobbies"}
     ];
   
     const gallery = document.getElementById("work-gallery");
