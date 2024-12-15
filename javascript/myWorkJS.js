d22314dafb1eaae07abb83ff04609c41c971d72f
Ari Guzzi <argu4451@colorado.edu>
2024-12-15 14:57:33 -0700

    fixed vr links

--- javascript/myWorkJS.js
+++ javascript/myWorkJS.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const works = [
         {"link": "webdev.html", "imgSrc": "../images/coverimages/webdev.png", "altText": "Web Development", "description": "Front-End Web Development"},
      {"link": "appdev.html", "imgSrc": "../images/coverimages/appdev.png", "altText": "App Development", "description": "App Development"},
-      {"link": "game.html", "imgSrc": "../images/coverimages/bayoubolt1.png", "altText": "Game Development", "description": "Tiny Game Development"},
       {"link": "vrdev.html", "imgSrc": "../images/coverimages/vrdev.png", "altText": "VR Development", "description": "VR Development"},
+      {"link": "game.html", "imgSrc": "../images/coverimages/bayoubolt1.png", "altText": "Game Development", "description": "Tiny Game Development"},
       {"link": "web.html", "imgSrc": "../images/coverimages/webdes.png", "altText": "Web Design", "description": "Web Design"},
       {"link": "appdes.html", "imgSrc": "../images/coverimages/appdes.png", "altText": "App Design", "description": "App Design"},
       {"link": "myhobbies.html", "imgSrc": "../images/coverimages/hobbies.png", "altText": "My Hobbies", "description": "My Hobbies"}
