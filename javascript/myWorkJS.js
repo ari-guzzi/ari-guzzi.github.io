aa1e4c03e7dde0c23bf0c0b637b13a69052d9b19
Ari Guzzi <arigzzi22@gmail.com>
2026-03-07 15:26:24 -0700

    actualize earth

--- javascript/myWorkJS.js
+++ javascript/myWorkJS.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const works = [
+      {"link": "actualize.html", "imgSrc": "../images/coverimages/actualize.png", "altText": "Actualize Earth", "description": "Actualize Earth Internship"},
       {"link": "finishd.html", "imgSrc": "../images/coverimages/finishdlogo.png", "altText": "finish'd internship", "description": "Redesigning finish'd, Inc.'s UI"},
       {"link": "capstone.html", "imgSrc": "../images/coverimages/capstonelogo.png", "altText": "Capstone Project", "description": "Capstone Project (App Dev)"},
       {"link": "webdev.html", "imgSrc": "../images/coverimages/webdev.png", "altText": "Web Development", "description": "Front-End Web Development"},
       {"link": "appdev.html", "imgSrc": "../images/coverimages/appdev.png", "altText": "App Development", "description": "App Development"},
       {"link": "vrdev.html", "imgSrc": "../images/coverimages/vrdev.png", "altText": "VR Development", "description": "VR Development"},
       {"link": "game.html", "imgSrc": "../images/coverimages/bayoubolt1.png", "altText": "Game Development", "description": "Tiny Game Development"},
-      {"link": "web.html", "imgSrc": "../images/coverimages/webdes.png", "altText": "Web Design", "description": "Web Design"},
-      {"link": "appdes.html", "imgSrc": "../images/coverimages/appdes.png", "altText": "App Design", "description": "App Design"},
+      // {"link": "web.html", "imgSrc": "../images/coverimages/webdes.png", "altText": "Web Design", "description": "Web Design"},
+      // {"link": "appdes.html", "imgSrc": "../images/coverimages/appdes.png", "altText": "App Design", "description": "App Design"},
       {"link": "myhobbies.html", "imgSrc": "../images/coverimages/hobbies.png", "altText": "My Hobbies", "description": "My Hobbies"}
     ];
   
